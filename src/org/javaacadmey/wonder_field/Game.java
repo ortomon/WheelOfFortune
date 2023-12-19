0e3140188f868c53f451e2c024e1b715b80ea3c5
alina <art.alina73@mail.ru>
2023-12-19 21:14:59 +0500

    added 1) Preparatory stage

---
+++ src/org/javaacadmey/wonder_field/Game.java
@@ -0,0 +1,44 @@
+package org.javaacadmey.wonder_field;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Game {
+    public static final int NUM_PLAYERS = 3;
+    public static final int TOTAL_ROUNDS = 4;
+    public static final int GROUP_ROUNDS = 3;
+    public static final int FINAL_ROUND_INDEX = 3;
+    private String[] questions;
+    private String[] answers;
+
+    public static final Scanner scanner = new Scanner(System.in);
+
+    // инициализация игры
+    public void init() {
+        questions = new String[TOTAL_ROUNDS];
+        answers = new String[TOTAL_ROUNDS];
+
+        System.out.println("Запуск игры \"Поле Чудес\" - подготовка к игре. Вам нужно ввести вопросы и ответы для игры.");
+
+        for (int i = 0; i < TOTAL_ROUNDS; i++) {
+            System.out.println("Введите вопрос #" + (i + 1));
+            questions[i] = scanner.nextLine();
+            System.out.println("Введите ответ на вопрос #" + (i + 1));
+            answers[i] = scanner.nextLine();
+        }
+        System.out.println("Иницализация закончена, игра начнется через 5 секунд");
+
+        // пауза 5 секунд
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Очистка консоли
+        for (int i = 0; i < 50; ++i) {
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/org/javaacadmey/wonder_field/Runner.java
@@ -0,0 +1,6 @@
+package org.javaacadmey.wonder_field;
+
+public class Runner {
+    public static void main(String[] args) {
+    }
+}
